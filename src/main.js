241341f368afab8765b3dc833c4be008c35dbaa0
uomma <marcelloleone975@gmail.com>
2024-01-10 16:09:30 +0100

    add: font nunito

--- src/main.js
+++ src/main.js
@@ -1,5 +1,6 @@
 import { createApp } from 'vue'
 import './assets/sass/main.scss';
+import "@fontsource/nunito"; // Defaults to weight 400
 import App from './App.vue'
 
 createApp(App).mount('#app')
